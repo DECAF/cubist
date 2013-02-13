c28b9b70a899613c3919e1afe99021d41689e00f
grmlin <shopping@hardwehr.de>
2013-02-13 14:06:34 +0100

    first cubist demo

---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+(function () {
+    var cube = new Cubist(document.getElementById('cubist'), {isRotatedVertically : false});
+
+    $('#cube-nav').on('click', 'ul li a', function (e) {
+        e.preventDefault();
+        i = $('#cube-nav').find('ul li a').index(this);
+        cube.goto(i);
+    }).on('click', '.next', function (e) {
+        e.preventDefault();
+        cube.next();
+    }).on('click', '.prev', function (e) {
+        e.preventDefault();
+        cube.previous();
+    }).on('click', '.first', function (e) {
+        e.preventDefault();
+        cube.first();
+    }).on('click', '.last', function (e) {
+        e.preventDefault();
+        cube.last();
+    });
+}());
